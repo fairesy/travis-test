7d55b01fe44f82735ae3924aef926c34620d06ba
Shinyoung <fairesy@gmail.com>
2016-04-18 15:53:24 +0900

    karma setting edit

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
     config.set({
         frameworks: ["mocha", "chai"],
         files: ["js/todo.js", "test/todo.test.js"],
-        browsers: ["PhantomJS", "Chrome"],
+        browsers: ["PhantomJS"],
         singleRun: true
     });
 };
